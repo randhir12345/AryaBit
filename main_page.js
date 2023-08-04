1de921ff7709c061f1bc55e75f48f550dafde15d
Randhir Kumar <krandhir895@gmail.com>
2023-08-04 09:02:59 +0530

    Add files via upload

---
+++ main_page.js
@@ -0,0 +1,25 @@
+topics = [
+    {
+        name: 'Structures',
+        link: 'structure.html'
+    },
+    {
+        name: 'Bitwise',
+        link: 'bitwise.html'
+    }
+]
+
+assignmentButtonElement = document.querySelector('.button-menu-container');
+assignmentHtml = '';
+
+topics.forEach((topic) => {
+    assignmentHtml += `
+        <div class="assignment-link-button">
+            <a class="assignment-link-section" href="${topic.link}">
+                ${topic.name}
+            </a>
+        </div>
+    `;
+});
+
+assignmentButtonElement.innerHTML = assignmentHtml;
